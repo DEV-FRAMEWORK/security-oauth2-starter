cf5cc04fc921cea57ac46b8f4fa43575880be54e
yonghui.park <yonghui.park@kt.com>
2019-01-20 23:02:37 +0900

    20190120-1

--- src/main/java/com/ktds/config/SecurityConfiguration.java
+++ src/main/java/com/ktds/config/SecurityConfiguration.java
@@ -1,12 +1,14 @@
 package com.ktds.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -16,10 +18,32 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+      //  PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        BCryptPasswordEncoder encoder = passwordEncoder();
+
         auth.inMemoryAuthentication()
                 .withUser("user1")
-                .password("12345678")
+                //.password("12345678")
+                .password(encoder.encode("12345678"))
+                //.passwordEncoder("12345678")
                 .roles("ADMIN", "USER");
+
+                int i = 0;
+                while (i < 5) {
+                    String password = "12345678";
+                    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+                    String hashedPassword = passwordEncoder.encode(password);
+
+                    System.out.println(hashedPassword);
+                    i++;
+                }
+            
+             
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
--- src/test/java/com/google/AuthApplicationTests.java
+++ src/test/java/com/google/AuthApplicationTests.java
@@ -1,16 +1,25 @@
 package com.google;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertTrue;
 
-@RunWith(SpringRunner.class)
+import org.junit.Test;
+//import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 public class AuthApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
-
+	
+	@Test
+	public void testAuthorization(){
+	Pbkdf2PasswordEncoder encoder = new Pbkdf2PasswordEncoder();
+	String result = encoder.encode("myPassword");
+	assertTrue(encoder.matches("myPassword", result));
+	}	
 }
